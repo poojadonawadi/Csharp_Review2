5e14757402fe9ca8a2c72be4d48c7786d8f9139c
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-21 10:19:52 +0530

    [Pooja]: added game starting point

---
+++ SnakeLadderGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review2
+{
+    internal class SnakeLadderGame
+    {
+        public static void PlayerStartPoint(int startPoint)
+        {
+            startPoint = 0;
+            Console.WriteLine("The start point of the Snake and Ladder game is: " + startPoint);
+        }
+    }
+}
